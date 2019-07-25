9f5d316e74eb101c9fdaf823e146101c2826e63a
Zaldivaa <52496921+Zaldivaa@users.noreply.github.com>
2019-07-24 22:38:16 -0400

    Add files via upload

---
+++ github/assets/data/myBooks.ts
@@ -0,0 +1,10 @@
+import { bookInfo } from "../../app/bookInfo";
+
+export const MYBOOKS: bookInfo[] =
+    [
+        { name: 'Art of War', author: 'Sun Tzu', genre: 'Fiction', year: '1080', picture: '../assets/images/ArtOfWar.jpg' },
+        { name: '1984', author: 'George Orwell', genre: 'Dystopian', year: '1949', picture: '../assets/images/1984.jpg' },
+        { name: 'Journey to the West', author: 'Wu Chengen', genre: 'Chinese Mythology', year: '1592 (First print)', picture: '../assets/images/West.jpg' },
+        { name: 'The Prince', author: 'Niccolo Machiavelli', genre: 'Non-Fiction', year: '1532', picture: '../assets/images/Prince.jpg' },
+        { name: 'Percy Jackson and the Olympians', author: 'Rick Riordan', genre: 'Fantasy', year: '2005', picture: '../assets/images/Percy.jpg' }
+    ]
